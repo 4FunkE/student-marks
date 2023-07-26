0bc1a2ab08cfaa055206ce5891f34b34e2d24e89
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-07-26 19:04:07 -0400

    Edit schema file

--- db/schema.sql
+++ db/schema.sql
@@ -4,20 +4,30 @@ CREATE DATABASE school_db;
 USE school_db;
 
 CREATE TABLE teachers (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     emails VARCHAR(50) NOT NULL,
     passwords VARCHAR(50) NOT NULL,
-    course VARCHAR(100)
+    course_id INT
 );
 
 CREATE TABLE courses (
-  id INT,
+  id INT AUTO_INCREMENT,
   course_title VARCHAR(30) NOT NULL,
   teachers_id INT,
+  student_id INT,
   course_details TEXT,
   FOREIGN KEY (teachers_id)
   REFERENCES teachers(id)
   ON DELETE SET NULL
+);
+
+
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    passwords VARCHAR(50) NOT NULL,
+    course_id INT
 );
