1e8205a9a0f93cc87c3fd0a5f56f5df6b687027b
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-07-26 20:26:00 -0400

    seeds file is complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,29 @@
-INSERT INTO teachers ()
+INSERT INTO teachers (first_name, last_name, email, course_id)
+VALUES 
+  ('Tyreal', 'Worley', 'tyreal.worley@example.com', 1),
+  ('Jane', 'Smith', 'jane.smith@example.com', 2),
+  ('Michael', 'Johnson', 'michael.johnson@example.com', NULL),
+  ('Emily', 'Williams', 'emily.williams@example.com', 3),
+  ('David', 'Brown', 'david.brown@example.com', NULL),
+  ('Sophia', 'Miller', 'sophia.miller@example.com', 2);
+
+
+INSERT INTO courses (id, course_title, teachers_id, students_id, course_details)
+VALUES (1, 'Mathematics', 1, NULL, 'Introduction to Mathematics'),
+       (2, 'Science', 2, NULL, 'Basic Science Concepts');
+       (3, 'English' 3, NULL 'Intro to english');
+       (4, 'history', 4, NULL, 'World history')
+
+INSERT INTO students (id, first_name, last_name, course_id)
+VALUES 
+  (1, 'Alice', 'Johnson', 1),
+  (2, 'Bob', 'Smith', 2),
+  (3, 'Eva', 'Brown', NULL),
+  (4, 'Michael', 'Williams', 1),
+  (5, 'Olivia', 'Davis', 3),
+  (6, 'James', 'Miller', NULL),
+  (7, 'Sophia', 'Anderson', 2),
+  (8, 'Alexander', 'Martinez', 3),
+  (9, 'Emma', 'Thomas', 1),
+  (10, 'Daniel', 'Lee', NULL);
+
