890bf1794d26ddfa83ec2db3ce500da2482b7be2
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-07-26 20:52:10 -0400

    fix seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,9 +10,9 @@ VALUES
 
 INSERT INTO courses (id, course_title, teachers_id, students_id, course_details)
 VALUES (1, 'Mathematics', 1, NULL, 'Introduction to Mathematics'),
-       (2, 'Science', 2, NULL, 'Basic Science Concepts');
-       (3, 'English' 3, NULL 'Intro to english');
-       (4, 'history', 4, NULL, 'World history')
+       (2, 'Science', 2, NULL, 'Basic Science Concepts'),
+       (3, 'English' 3, NULL 'Intro to english'),
+       (4, 'history', 4, NULL, 'World history');
 
 INSERT INTO students (id, first_name, last_name, course_id)
 VALUES 
