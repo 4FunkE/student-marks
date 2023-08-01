7b8e18179927f3a775f9b7d0c7125934c2513b60
jaldhara.patel <jaldharapatel91@gmail.com>
2023-07-31 20:27:23 -0400

    change code in js files and json

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,5 @@ CREATE DATABASE studentmark_db;
 
 USE studentmark_db;
 
-CREATE TABLE teachers (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    course_id INT
-);
-
-CREATE TABLE students (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    course_id INT
-);
-
-CREATE TABLE courses (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  course_title VARCHAR(30) NOT NULL,
-  teachers_id INT,
-  students_id INT,
-  course_details TEXT,
-  FOREIGN KEY (teachers_id)
-  REFERENCES teachers(id)
-  ON DELETE SET NULL,
-  FOREIGN KEY (students_id) REFERENCES students(id) ON DELETE SET NULL
-);
 
 
--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,6 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#teacher-name').value.trim();
-  // const needed_funding = document.querySelector('#project-funding').value.trim();
   const description = document.querySelector('#rating-desc').value.trim();
   const selectedValue = localStorage.getItem('selectedValue');
   console.log("Retrieved value from localStorage:", selectedValue);
