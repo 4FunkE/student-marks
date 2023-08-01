ba48fd90a5fe25726a93612bc1f4c93e1aeed40b
4FunkE <4FunkEcodes@gmail.com>
2023-08-01 12:29:22 -0400

    new emojis for random helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,23 +1,16 @@
 module.exports = {
-  format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-  // we do not need format amount
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
   get_emoji: () => {
     const randomNum = Math.random();
 
     // Return a random emoji CHANGE THE EMOJI!!!
     if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
+      return '<span for="img" aria-label="book">📖</span>';
     } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
+      return '<span for="img" aria-label="pencil">✏️</span>';
+    } else if (randomNum > 0.1) {
+      return '<span for="img" aria-label="school">🏫</span>';
     } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
+      return '<span for="img" aria-label="teacher">🤓</span>';
     }
   },
 };
